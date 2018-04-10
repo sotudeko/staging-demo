7420d1e18df55b315975667d7de4df7626865b42
sotudeko <sotudeko@sonatype.com>
2018-04-10 17:33:18 +0100

    1.0.17

--- staging_info.sh
+++ staging_info.sh
@@ -3,5 +3,5 @@
 repo=$1
 tag=$2
 
-curl -X GET --user admin:admin123 http://localhost:8081/service/rest/beta/tags | python -m json.tool
+curl -s -X GET --user admin:admin123 http://localhost:8081/service/rest/beta/tags | python -m json.tool
 #curl  -X GET --user admin:admin123 'http://localhost:8081/service/rest/beta/search?repository=${repo}&tag=${tag}''
