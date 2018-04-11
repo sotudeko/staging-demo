bb042a145c72ff269367d31709f91f0c669e42bf
sotudeko <sotudeko@sonatype.com>
2018-04-11 17:20:36 +0100

    1.0.31

---
+++ staging_associate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tag=$1
+ver=$2
+
+#curl -s -X POST -u admin:admin123 -H 'Content-Type: application/json' -H 'Accept: application/json' 'http://localhost:8081/service/rest/beta/tags/associate/jenkins-WebGoat-Example-Staging-Demo-31?repository=mvn-releases&maven.groupId=WebGoat&maven.baseVersion=5.5.16&maven.artifactId=WebGoat'
+
+curl -s -X POST -u admin:admin123 -H 'Content-Type: application/json' -H 'Accept: application/json' \
+http://localhost:8081/service/rest/beta/tags/associate/${tag}? \
+-d repository=mvn-releases \
+-d maven.groupId=WebGoat \
+-d maven.baseVersion=${ver} \
+-d maven.artifactId=WebGoat
