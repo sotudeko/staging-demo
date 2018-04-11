750dd2e3771f18ba11f1cceb3b87fb054ded17a2
sotudeko <sotudeko@sonatype.com>
2018-04-11 16:32:55 +0100

    1.0.30

--- staging_generate_tag.sh
+++ staging_generate_tag.sh
@@ -6,7 +6,7 @@ echo ' "attributes": { '
 echo "    \"buildUser\": \"$1\"," 
 echo "    \"buildJob\": \"$2\","
 echo "    \"buildId\": \"$3\"," 
-echo "    \"buildUrl\": \"$4\"" 
+echo "    \"buildUrl\": \"$4\"," 
 echo "    \"appVersion\": \"$6\"" 
 echo '  }'
 echo '}'
